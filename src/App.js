0f676a8dd65802f9c4bef5971b57b98cd27cae40
kakon4366 <kakonbarman4366@gmail.com>
2022-04-06 00:00:53 +0600

    User authentication by google

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
 import app from "./firebase.init";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const auth = getAuth(app);
 
 function App() {
-	return <div className="App"></div>;
+	const provider = new GoogleAuthProvider();
+
+	const handleGoogleAuth = () => {
+		signInWithPopup(auth, provider)
+			.then((result) => {
+				const user = result.user;
+				console.log(user);
+			})
+			.catch((error) => console.log(error));
+	};
+
+	return (
+		<div className="App">
+			<button onClick={handleGoogleAuth}>Google</button>
+		</div>
+	);
 }
 
 export default App;
